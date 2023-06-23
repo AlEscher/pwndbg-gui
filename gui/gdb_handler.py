53e9bcf6d155b31dfd33136a0827a347e57249b2
maxp <maximilian.pursche@tum.de>
2023-06-23 23:26:26 +0200

    shit that took 2 hours to accomplish nothing well done gdb, (fk sucks)

--- gui/gdb_handler.py
+++ gui/gdb_handler.py
@@ -6,6 +6,22 @@ import gdb
 from PySide6.QtCore import QObject, Slot, Signal
 from pwndbg.commands.context import context_stack, context_regs, context_disasm, context_code, context_backtrace
 
+import PySide6.QtCore
+import sys
+from io import StringIO
+
+# shit that doesnt work
+#qfile = PySide6.QtCore.QFile()
+#qfile.open(sys.__stdout__.fileno(), PySide6.QtCore.QFile.OpenMode(0))
+#GDB_OUT = PySide6.QtCore.QTextStream(qfile)
+
+# Create a StringIO object to capture the stdout
+GDB_OUT = StringIO()
+# Redirect sys.stdout to the captured_stdout
+#sys.__stdout__ = GDB_OUT
+sys.stdout = GDB_OUT
+
+
 logger = logging.getLogger(__file__)
 
 
@@ -21,7 +37,9 @@ class GdbHandler(QObject):
     @Slot()
     def send_command(self, cmd: str):
         response = gdb.execute(cmd, from_tty=True, to_string=True)
-        self.update_gui.emit("main", response.encode())
+        main_response = GDB_OUT.getvalue()
+        logger.info("logged from __stdout__: %s", main_response)
+        self.update_gui.emit("main", main_response.encode() + response.encode())
         # Update contexts
         for context, func in self.context_to_func.items():
             if context in self.active_contexts:
