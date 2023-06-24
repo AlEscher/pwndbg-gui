9a2071729606307bece1e09f34aeb96f47cc84f5
Alessandro <alessandro.escher@tum.de>
2023-06-24 17:55:20 +0200

    Fix certain characters getting removed by accident

--- gui/parser.py
+++ gui/parser.py
@@ -34,28 +34,28 @@ class ContextParser:
         # Colors
         if start == b"30m":
             self.parser.setTextColor(Qt.GlobalColor.black)
-            self.parser.insertPlainText(token.strip(start).decode())
+            self.parser.insertPlainText(token.replace(start, b"", 1).decode())
         elif start == b"31m":
             self.parser.setTextColor(Qt.GlobalColor.darkRed)
-            self.parser.insertPlainText(token.strip(start).decode())
+            self.parser.insertPlainText(token.replace(start, b"", 1).decode())
         elif start == b"32m":
             self.parser.setTextColor(Qt.GlobalColor.darkGreen)
-            self.parser.insertPlainText(token.strip(start).decode())
+            self.parser.insertPlainText(token.replace(start, b"", 1).decode())
         elif start == b"33m":
             self.parser.setTextColor(Qt.GlobalColor.darkYellow)
-            self.parser.insertPlainText(token.strip(start).decode())
+            self.parser.insertPlainText(token.replace(start, b"", 1).decode())
         elif start == b"34m":
             self.parser.setTextColor(Qt.GlobalColor.darkBlue)
-            self.parser.insertPlainText(token.strip(start).decode())
+            self.parser.insertPlainText(token.replace(start, b"", 1).decode())
         elif start == b"35m":
             self.parser.setTextColor(Qt.GlobalColor.darkMagenta)
-            self.parser.insertPlainText(token.strip(start).decode())
+            self.parser.insertPlainText(token.replace(start, b"", 1).decode())
         elif start == b"36m":
             self.parser.setTextColor(Qt.GlobalColor.darkCyan)
-            self.parser.insertPlainText(token.strip(start).decode())
+            self.parser.insertPlainText(token.replace(start, b"", 1).decode())
         elif start == b"37m":
             self.parser.setTextColor(Qt.GlobalColor.white)
-            self.parser.insertPlainText(token.strip(start).decode())
+            self.parser.insertPlainText(token.replace(start, b"", 1).decode())
         elif start.startswith(b"38"):
             # 256-bit color format: 38;5;<FG COLOR>m
             # RGB format: 38;2;<r>;<g>;<b>m
@@ -71,7 +71,7 @@ class ContextParser:
                 self.parser.setTextColor(QColor.fromRgb(r, g, b, 255))
             # Add the "m" into the start for stripping
             start = token[:token.index(b"m")+1]
-            self.parser.insertPlainText(token.strip(start).decode())
+            self.parser.insertPlainText(token.replace(start, b"", 1).decode())
         elif start == b"39m":
             self.parser.setTextColor(Qt.GlobalColor.white)
             self.parser.insertPlainText(token.strip(start).decode())
@@ -95,6 +95,7 @@ class ContextParser:
             self.parser.insertPlainText(token.decode())
 
 
+# ChatGPT:
 def convert_8bit_to_32bit(rgb_8bit):
     # Extracting the 8-bit color components
     red = (rgb_8bit >> 5) & 0b111  # 3 bits for red
