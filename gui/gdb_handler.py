96a2d5b7345bb14e248dc3825076d6c993a261f5
maxp <maximilian.pursche@tum.de>
2023-06-24 17:03:26 +0200

    implemented events

--- gui/gdb_handler.py
+++ gui/gdb_handler.py
@@ -6,22 +6,6 @@ import gdb
 from PySide6.QtCore import QObject, Slot, Signal
 from pwndbg.commands.context import context_stack, context_regs, context_disasm, context_code, context_backtrace
 
-import PySide6.QtCore
-import sys
-from io import StringIO
-
-# shit that doesnt work
-#qfile = PySide6.QtCore.QFile()
-#qfile.open(sys.__stdout__.fileno(), PySide6.QtCore.QFile.OpenMode(0))
-#GDB_OUT = PySide6.QtCore.QTextStream(qfile)
-
-# Create a StringIO object to capture the stdout
-GDB_OUT = StringIO()
-# Redirect sys.stdout to the captured_stdout
-#sys.__stdout__ = GDB_OUT
-sys.stdout = GDB_OUT
-
-
 logger = logging.getLogger(__file__)
 
 
@@ -31,15 +15,14 @@ class GdbHandler(QObject):
     def __init__(self, active_contexts: List[str]):
         super().__init__()
         self.past_commands: List[str] = []
-        self.context_to_func = dict(regs=context_regs, stack=context_stack, disasm=context_disasm, code=context_code, backtrace=context_backtrace)
+        self.context_to_func = dict(regs=context_regs, stack=context_stack, disasm=context_disasm, code=context_code,
+                                    backtrace=context_backtrace)
         self.active_contexts = active_contexts
 
     @Slot()
     def send_command(self, cmd: str):
         response = gdb.execute(cmd, from_tty=True, to_string=True)
-        main_response = GDB_OUT.getvalue()
-        logger.info("logged from __stdout__: %s", main_response)
-        self.update_gui.emit("main", main_response.encode() + response.encode())
+        self.update_gui.emit("main", response.encode())
         # Update contexts
         for context, func in self.context_to_func.items():
             if context in self.active_contexts:
@@ -52,3 +35,39 @@ class GdbHandler(QObject):
         logger.info("Setting GDB target to %s", arguments)
         cmd = " ".join(arguments)
         gdb.execute(cmd)
+
+
+def cont_handler(event):
+    logger.info("event type: continue (inferior runs)")
+
+
+def exit_handler(event):
+    logger.info("event type: exit (inferior exited)")
+    if hasattr(event, 'exit_code'):
+        logger.info("exit code: %d" % event.exit_code)
+    else:
+        logger.info("exit code not available")
+
+
+def stop_handler(event):
+    logger.info("event type: stop (inferior stopped)")
+    if hasattr(event, 'breakpoints'):
+        logger.info("hit breakpoint(s): %d" % event.breakpoints[0].number)
+        logger.info("at %s", event.breakpoints[0].location)
+        logger.info("hit count: %d", event.breakpoints[0].hit_count)
+    else:
+        logger.info("no breakpoint was hit")
+
+
+def call_handler(event):
+    logger.info("event type: call (inferior calls function)")
+    if hasattr(event, 'address'):
+        logger.info("function to be called at: %s" % hex(event.address))
+    else:
+        logger.info("function address not available")
+
+
+gdb.events.cont.connect(cont_handler)
+gdb.events.exited.connect(exit_handler)
+gdb.events.stop.connect(stop_handler)
+gdb.events.inferior_call.connect(call_handler)
