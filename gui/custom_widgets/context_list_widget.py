33e4279f5345d1db64ecac7b334da1e990a1774e
Alessandro <alessandro.escher@tum.de>
2023-06-24 19:08:10 +0200

    Add explanation

--- gui/custom_widgets/context_list_widget.py
+++ gui/custom_widgets/context_list_widget.py
@@ -37,4 +37,6 @@ class ContextListWidget(QListWidget):
         # Remove unneeded lines containing only HTML, as "content" will be a full HTML document returned by the parser
         body_start = lines.index(next(line for line in lines if "<body" in line))
         for line in lines[body_start + 1:]:
-            QListWidgetItem(delete_first_html_tag(delete_last_html_tag(line)), self)
+            # Remove <p...></p> tag
+            cleaned = delete_first_html_tag(delete_last_html_tag(line))
+            QListWidgetItem(cleaned, self)
