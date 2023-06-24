8243012ea2f481317e2d6d433eb44c392661fab7
maxp <maximilian.pursche@tum.de>
2023-06-24 18:52:00 +0200

    removing <p> tags in list

--- gui/custom_widgets/context_list_widget.py
+++ gui/custom_widgets/context_list_widget.py
@@ -1,10 +1,30 @@
 from typing import TYPE_CHECKING
+import re
 
 from PySide6.QtWidgets import QListWidget, QListWidgetItem
 
 # Prevent circular import error
 if TYPE_CHECKING:
     from gui.gui import PwnDbgGui
+
+
+def delete_first_html_tag(string):
+    pattern = r"<[^>]+>"  # Regular expression pattern to match HTML tags
+    return re.sub(pattern, "", string, count=1)
+
+
+def delete_last_html_tag(string):
+    # Find the last closing HTML tag
+    pattern = r'</[^>]+>$'
+    match = re.search(pattern, string)
+
+    if match:
+        last_tag = match.group()
+        # Remove the last closing HTML tag
+        modified_string = string.replace(last_tag, '')
+        return modified_string
+    else:
+        return string  # No closing HTML tag found
 
 
 class ContextListWidget(QListWidget):
@@ -16,6 +36,5 @@ class ContextListWidget(QListWidget):
         lines = content.splitlines()
         # Remove unneeded lines containing only HTML, as "content" will be a full HTML document returned by the parser
         body_start = lines.index(next(line for line in lines if "<body" in line))
-        for line in lines[body_start+1:]:
-            QListWidgetItem(line, self)
-
+        for line in lines[body_start + 1:]:
+            QListWidgetItem(delete_first_html_tag(delete_last_html_tag(line)), self)
