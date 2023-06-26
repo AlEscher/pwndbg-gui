cb25119a2269877bfc2544f41de7080a5a5594c3
Alessandro <alessandro.escher@tum.de>
2023-06-26 15:48:50 +0200

    Change logging format, logging to file does not work (makes gdb crash)

--- gui/start.py
+++ gui/start.py
@@ -2,7 +2,7 @@ import logging
 
 from gui.gui import run_gui
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s | [%(levelname)s] : %(message)s')
 logger = logging.getLogger(__file__)
 
 
