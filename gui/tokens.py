355b68c3104247e43b189ee5d096276fad5c345f
Alessandro <alessandro.escher@tum.de>
2023-06-29 11:37:26 +0200

    Rename tokens, added comments

--- gui/tokens.py
+++ gui/tokens.py
@@ -2,22 +2,27 @@ from enum import Enum
 
 
 class ResponseToken(Enum):
+    """Tokens used to identify commands given to the GDB Machine Interface and decide what to do with the result.
+    The first part of the token name shows who triggered the command, the second part denotes its destination"""
+    # Discard result
     DELETE = 0
-    MAIN_USER = 1
-    MAIN_GUI = 2
-    DISASM_GUI = 3
-    CODE_GUI = 4
-    REGS_GUI = 5
-    STACK_GUI = 6
-    BACKTRACE_GUI = 7
+    # Command originated by user, add to main output widget
+    USER_MAIN = 1
+    # Command originated by us, add to main output widget
+    GUI_MAIN_CONTEXT = 2
+    GUI_DISASM_CONTEXT = 3
+    GUI_CODE_CONTEXT = 4
+    GUI_REGS_CONTEXT = 5
+    GUI_STACK_CONTEXT = 6
+    GUI_BACKTRACE_CONTEXT = 7
 
 
 Token_to_Context = {
-    ResponseToken.MAIN_USER.value(): "main",
-    ResponseToken.MAIN_GUI.value(): "main",
-    ResponseToken.DISASM_GUI.value(): "disasm",
-    ResponseToken.CODE_GUI.value(): "code",
-    ResponseToken.REGS_GUI.value(): "regs",
-    ResponseToken.BACKTRACE_GUI.value(): "backtrace",
-    ResponseToken.STACK_GUI.value(): "stack",
+    ResponseToken.USER_MAIN.value(): "main",
+    ResponseToken.GUI_MAIN_CONTEXT.value(): "main",
+    ResponseToken.GUI_DISASM_CONTEXT.value(): "disasm",
+    ResponseToken.GUI_CODE_CONTEXT.value(): "code",
+    ResponseToken.GUI_REGS_CONTEXT.value(): "regs",
+    ResponseToken.GUI_BACKTRACE_CONTEXT.value(): "backtrace",
+    ResponseToken.GUI_STACK_CONTEXT.value(): "stack",
 }
