baec07b1ae0bc877ef3d19087be243578c36bbf4
Alessandro <alessandro.escher@tum.de>
2023-06-29 16:48:15 +0200

    Change ResponseToken to IntEnum

--- gui/tokens.py
+++ gui/tokens.py
@@ -1,7 +1,7 @@
-from enum import Enum
+from enum import IntEnum
 
 
-class ResponseToken(Enum):
+class ResponseToken(IntEnum):
     """Tokens used to identify commands given to the GDB Machine Interface and decide what to do with the result.
     The first part of the token name shows who triggered the command, the second part denotes its destination"""
     # Discard result
@@ -15,19 +15,23 @@ class ResponseToken(Enum):
     GUI_REGS_CONTEXT = 5
     GUI_STACK_CONTEXT = 6
     GUI_BACKTRACE_CONTEXT = 7
+    GUI_HEAP_HEAP = 8
+    GUI_HEAP_BINS = 9
+    GUI_HEAP_TRY_MALLOC = 10
+    GUI_HEAP_TRY_FREE = 11
 
     def __str__(self):
         return str(self.value)
 
 
 Token_to_Context = {
-    ResponseToken.USER_MAIN.value: "main",
-    ResponseToken.GUI_MAIN_CONTEXT.value: "main",
-    ResponseToken.GUI_DISASM_CONTEXT.value: "disasm",
-    ResponseToken.GUI_CODE_CONTEXT.value: "code",
-    ResponseToken.GUI_REGS_CONTEXT.value: "regs",
-    ResponseToken.GUI_BACKTRACE_CONTEXT.value: "backtrace",
-    ResponseToken.GUI_STACK_CONTEXT.value: "stack",
+    ResponseToken.USER_MAIN: "main",
+    ResponseToken.GUI_MAIN_CONTEXT: "main",
+    ResponseToken.GUI_DISASM_CONTEXT: "disasm",
+    ResponseToken.GUI_CODE_CONTEXT: "code",
+    ResponseToken.GUI_REGS_CONTEXT: "regs",
+    ResponseToken.GUI_BACKTRACE_CONTEXT: "backtrace",
+    ResponseToken.GUI_STACK_CONTEXT: "stack",
 }
 
 Context_to_Token = dict(map(reversed, Token_to_Context.items()))
