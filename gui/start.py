51504d672483038c32892c6914722142dab38b05
Alessandro <alessandro.escher@tum.de>
2023-06-30 12:21:35 +0200

    Fix start.py include path, can now be started from anywhere

--- gui/start.py
+++ gui/start.py
@@ -4,6 +4,7 @@ import sys
 from os import path
 directory, file = path.split(__file__)
 directory = path.expanduser(directory)
+directory = path.join(directory, "..")
 directory = path.abspath(directory)
 sys.path.append(directory)
 
