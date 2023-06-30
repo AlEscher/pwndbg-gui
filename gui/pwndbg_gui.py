50183603d39ec7a053644c8ed6d8af4fa311683b
maxp <maximilian.pursche@tum.de>
2023-06-30 11:36:36 +0200

    removed logging + main scrolls to bottom

--- gui/pwndbg_gui.py
+++ gui/pwndbg_gui.py
@@ -238,7 +238,6 @@ class PwnDbgGui(QMainWindow):
         if context == "main":
             remove_header = False
             # Main should end with newline
-            logger.debug(content)
             if content != b"" and not content.endswith(b"\n"):
                 content += b"\n"
         html = self.parser.to_html(content, remove_header)
