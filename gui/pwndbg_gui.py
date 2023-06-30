c2f5215cd37e8668b9a50a0ec7eb6be4ca71f138
Alessandro <alessandro.escher@tum.de>
2023-06-30 10:46:52 +0200

    Added tooltips

--- gui/pwndbg_gui.py
+++ gui/pwndbg_gui.py
@@ -110,20 +110,20 @@ class PwnDbgGui(QMainWindow):
         debug_toolbar = self.addToolBar("Debug")
 
         start_action = QAction("Start Program", self)
-        start_action.setStatusTip("Start the program to debug")
+        start_action.setToolTip("Start the program to debug")
         start_action.setShortcut(QKeySequence.StandardKey.New)
         start_action.triggered.connect(self.select_file)
         debug_menu.addAction(start_action)
         debug_toolbar.addAction(start_action)
 
         attach_name_action = QAction("Attach Via Name", self)
-        attach_name_action.setStatusTip("Attach to a running program via its name (must be unique)")
+        attach_name_action.setToolTip("Attach to a running program via its name (requires sudo)")
         attach_name_action.triggered.connect(self.query_process_name)
         debug_menu.addAction(attach_name_action)
         debug_toolbar.addAction(attach_name_action)
 
         attach_pid_action = QAction("Attach Via PID", self)
-        attach_pid_action.setStatusTip("Attach to a running program via its pid")
+        attach_pid_action.setToolTip("Attach to a running program via its pid (requires sudo)")
         attach_pid_action.triggered.connect(self.query_process_pid)
         debug_menu.addAction(attach_pid_action)
         debug_toolbar.addAction(attach_pid_action)
@@ -131,7 +131,7 @@ class PwnDbgGui(QMainWindow):
         debug_menu.addSeparator()
         exit_action = QAction("E&xit", self)
         exit_action.setShortcut(QKeySequence.StandardKey.Quit)
-        exit_action.setStatusTip("Exit the application")
+        exit_action.setToolTip("Exit the application")
         exit_action.triggered.connect(self.close)
         debug_menu.addAction(exit_action)
 
