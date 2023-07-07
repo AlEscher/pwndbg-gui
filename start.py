316b1f2619b69b2bff175db990a0e9f7c9c457f6
Alessandro <alessandro.escher@tum.de>
2023-07-07 12:10:56 +0200

    Added log level to start that will be forwarded to GUI

--- start.py
+++ start.py
@@ -6,7 +6,7 @@ import sys
 import venv
 from pathlib import Path
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s | [%(levelname)s] : %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s | [%(levelname)s] : %(message)s')
 logger = logging.getLogger(__file__)
 
 
@@ -24,12 +24,12 @@ def install_dependencies(env_dir: Path, requirements_file: Path):
     subprocess.run([pip_path, "install", "-r", requirements_file], check=True)
 
 
-def run_script_in_environment(env_dir: Path, script_path: Path, use_sudo: bool):
+def run_script_in_environment(env_dir: Path, script_path: Path, args: argparse.Namespace):
     """Run the Python script within the virtual environment."""
     python_path = f"{env_dir}/Scripts/python" if sys.platform == "win32" else f"{env_dir}/bin/python"
     logger.info("Starting GUI using venv %s", str(env_dir))
-    cmd = [python_path, script_path]
-    if use_sudo:
+    cmd = [python_path, script_path, args.log]
+    if args.sudo:
         cmd = ["sudo", "-S"] + cmd
         password = getpass.getpass("Enter your sudo password: ")
         password = password.strip()
@@ -43,6 +43,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--sudo", action="store_true", help="Run the script with sudo. This is required if you want "
                                                             "GDB to attach to a running process")
+    parser.add_argument("-log", help="Set the log level of the GUI", default="INFO")
     args = parser.parse_args()
     root_dir = Path(__file__).parent.resolve()
     env_dir = root_dir / "pwndbg-gui-venv"
@@ -51,7 +52,7 @@ def main():
 
     create_virtual_environment(env_dir)
     install_dependencies(env_dir, requirements_file)
-    run_script_in_environment(env_dir, script_path, args.sudo)
+    run_script_in_environment(env_dir, script_path, args)
 
 
 if __name__ == "__main__":
